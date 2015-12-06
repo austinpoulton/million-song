18a90dbaae7fdca62586f0e7dba06ac4a415a711
ec15287 <j.a.butcher@se15.qmul.ac.uk>
2015-12-06 16:39:16 +0000

    updates to runSparkJob.sh and SongCount.scala to work independent of username

--- src/main/scala/jiba/msd/analysis/SongCount.scala
+++ src/main/scala/jiba/msd/analysis/SongCount.scala
@@ -3,7 +3,7 @@ package jiba.msd.analysis
 import jiba.msd.model.Track
 import org.apache.spark._
 import org.apache.spark.SparkContext._
-import org.apache.spark.rdd.RDD._
+// import org.apache.spark.rdd.RDD._
 
 
 /**
@@ -11,15 +11,34 @@ import org.apache.spark.rdd.RDD._
  */
 object SongCount {
 
+  val moon = "hdfs://moonshot-ha-nameservice"
   val fileNames : Array[String] =  Array("A.csv", "B.csv", "C.csv"  )
   //val fileSizes : Map[String,Long]
 
   def main(args: Array[String]): Unit = {
     val sc = new SparkContext(new SparkConf().setAppName("Artist song count"))
+    val hdcf = new org.apache.hadoop.conf.Configuration()
+    val hdfs = org.apache.hadoop.fs.FileSystem.get(new java.net.URI(moon), hdcf)
+    val username = System.getProperties().get("user.name")
+    val outdir = moon + "/user/" + username + "/msd"
 
-    val lines = sc.textFile("hdfs://moonshot-ha-nameservice/data/millionsong/A.csv")
+    val lines = sc.textFile(moon + "/data/millionsong/A.csv")
     val tracks  = lines.map(line => Track.processTrackLine(line))
-    val aristSongCounts = tracks.map(t => (t.artistName, 1)).reduceByKey((v1, v2) => v1 + v2)
-    aristSongCounts.saveAsTextFile("hdfs://moonshot-ha-nameservice/user/agp30/")
+    val artistSongCounts = tracks.map(t => (t.artistName, 1)).reduceByKey((v1, v2) => v1 + v2)
+
+    //
+    // Zap the output directory first if it exists
+    //
+    try {
+       hdfs.delete(new org.apache.hadoop.fs.Path(outdir), true)
+        }
+    catch {
+       case _ : Throwable => { }
+          }
+    //
+    // Write out the result to user hdfs
+    //
+    println ("Saving to " + outdir)
+    artistSongCounts.saveAsTextFile(outdir)
   }
 }
